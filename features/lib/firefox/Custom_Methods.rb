aa6d14f6f2898a598a4744658e83024501edaae0
shanku_elmo <shanku.mahanty@elmolearning.com.au>
2016-06-20 14:19:15 +1000

    adding refactored libraries

---
+++ features/lib/firefox/Custom_Methods.rb
@@ -0,0 +1,28 @@
+module Firefox
+  module Custom_Methods
+
+    def WaitForDropdownByClassAndTouchTheIndex(class_name,index_value)
+      begin
+        wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+        select_item = wait.until {
+          element = $driver.find_elements(:class, "#{class_name}")[index_value]
+          element if element.displayed?
+        }
+        select_item.click
+      rescue Exception => e
+        puts e.message
+        $driver.quit
+      end
+    end
+    def PressEnter()
+      $driver.action.send_keys(:enter).perform
+    end
+    def TouchDelete()
+      element = $driver.find_element(:link, "Delete")
+      element.click
+    end
+    def Wait(timeout)
+      $driver.manage.timeouts.implicit_wait = timeout
+    end
+  end
+end
