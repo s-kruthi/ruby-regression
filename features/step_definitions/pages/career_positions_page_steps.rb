a2b01b47245c43d11907d7643e41b685ac316a49
Omar Khan <omar.khan@elmolearning.com.au>
2016-06-28 14:37:57 +1000

    Added .gitignore file

--- features/step_definitions/pages/career_positions_page_steps.rb
+++ features/step_definitions/pages/career_positions_page_steps.rb
@@ -5,18 +5,18 @@ def GoToPositionsUnderCareersTab(careers_tab,positions_link)
   WaitForAnElementByPartialLinkTextAndTouch(positions_link)
 end
 
-class SortingException < Exception;
+class CustomException < Exception;
+  $sortingError = "The list is not sorted"
 end
 
 def WaitForThePositionsListingAndVerifyTheAlphabeticalSorting()
-    pos_items = $driver.find_elements(:tag_name, "tr")[1..10]
+    pos_items = $driver.find_elements(:tag_name, "tr")[0..10]
   compare_pos_items = pos_items.map { |list| list.text.split("\nCompare")}
   new_pos_items = compare_pos_items.sort
   if
       (compare_pos_items == new_pos_items) == true
   else
-    raise SortingException.new("Sorting does not exist!")
-   end
-
+              raise CustomException.new($sortingError)
+  end
 
 end
