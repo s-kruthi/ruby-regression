e6376beb7126105a4ea71e3a7feb4d7080368cf8
Omar Khan <omar.khan@elmolearning.com.au>
2016-06-28 13:57:46 +1000

    Improved the sorting method to display an exception instead of passing the cucumber scenario

--- features/step_definitions/pages/career_positions_page_steps.rb
+++ features/step_definitions/pages/career_positions_page_steps.rb
@@ -1,4 +1,3 @@
-
 def GoToPositionsUnderCareersTab(careers_tab,positions_link)
   sleep(1)
   WaitForAnElementByPartialLinkTextAndTouch(careers_tab)
@@ -6,22 +5,18 @@ def GoToPositionsUnderCareersTab(careers_tab,positions_link)
   WaitForAnElementByPartialLinkTextAndTouch(positions_link)
 end
 
-def WaitForThePositionsListingAndVerifyTheAlphabeticalSorting()
-  #get the element list of positions
-    pos_items = $driver.find_elements(:tag_name, "tr")[1..10]
-#get the text for actual positions i that list
-  compare_pos_items = pos_items.map { |list| list.text.split("\nCompare")}
-#compare the listed positions with the sorted positions
-  new_pos_items = compare_pos_items.sort
-if
-         (compare_pos_items == new_pos_items) == true
-  puts "PASSED,SORTED ALPHABETICALLY!"
+class SortingException < Exception;
+end
 
-else
-  #doing this just to fail this scenario as ruby wont fail , need to find a long term solution
-  puts "FAILED  Because the list is not sorted alphabetically"
-  $driver.find_element()
-  end
+def WaitForThePositionsListingAndVerifyTheAlphabeticalSorting()
+    pos_items = $driver.find_elements(:tag_name, "tr")[1..10]
+  compare_pos_items = pos_items.map { |list| list.text.split("\nCompare")}
+  new_pos_items = compare_pos_items.sort
+  if
+      (compare_pos_items == new_pos_items) == true
+  else
+    raise SortingException.new("Sorting does not exist!")
+   end
 
 
 end
