4c46fc4a9bad960d2ffe2ac597a8ba0023e2662e
Omar Khan <omar.khan@elmolearning.com.au>
2016-11-30 18:22:39 +1100

    Updated chromedriver path in the config

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,7 @@ require "./features/lib/device_method_factory.rb"
 
 puts "CHANNEL = " + ENV["CHANNEL"]
 
-#ENV['CHANNEL'] = 'safari'
+#ENV['CHANNEL'] = 'chrome'
 
 WAIT_TIMEOUT = 5
 World(DeviceMethodFactory.for_channel(ENV['CHANNEL']))
