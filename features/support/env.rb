1895da386e72dc207421dc09395ebc57bebce005
shanku_elmo <shankumahanty@elmolearning.com.au>
2017-06-02 14:04:45 +1000

    adding env change modifications

---
+++ features/support/env.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'active_record'
+require 'touch_action/selenium-webdriver'
+require 'securerandom'
+require 'waitutil'
+require 'selenium-webdriver'
+require 'pry'
+require 'rest-client'
+require 'ruby-jmeter'
+
+require './features/lib/device_method_factory.rb'
+
+if ENV["CHANNEL"] == nil
+    ENV['CHANNEL'] = 'firefox' #Edit this when using RubyMine needs to run against specific browser
+  else
+    puts "CHANNEL = " + ENV["CHANNEL"]
+  end
+
+WAIT_TIMEOUT = 5
+World(DeviceMethodFactory.for_channel(ENV['CHANNEL']))
