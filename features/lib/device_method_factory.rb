9e390926717ff20e046fb0071293d4170b5200c2
Kruthi <kruthi.murthy@elmolearning.com.au>
2018-03-20 17:42:15 +1100

    Adding the database access files

--- features/lib/device_method_factory.rb
+++ features/lib/device_method_factory.rb
@@ -2,7 +2,7 @@ class DeviceMethodFactory
 
   def self.for_channel channel_name
     require "./features/lib/#{channel_name}/method_factory"
-    require "../../features/lib/Database/database_env"
+    require "./features/lib/database/database_env"
 
     if channel_name == "android"
         return Android::MethodFactory
