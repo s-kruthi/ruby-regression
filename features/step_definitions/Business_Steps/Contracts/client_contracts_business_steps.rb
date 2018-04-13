62b5444ad07ec4cd947e24fe0433a21ac32dbf41
omar.khan <omar.khan@elmolearning.com.au>
2018-04-13 17:04:24 +1000

    Renamed several method names to match current naming convention

--- features/step_definitions/Business_Steps/Contracts/client_contracts_business_steps.rb
+++ features/step_definitions/Business_Steps/Contracts/client_contracts_business_steps.rb
@@ -7,12 +7,12 @@ And(/^I Enter New Client Contract Details$/i) do
   Sleep_Until(EnterContractCode(CLIENT_CONTRACT_ID, CLIENT_CONTRACT_VALUE))
   Sleep_Until(EnterIsActiveStatus(CLIENT_CONTRACT_STATUS_ID))
   Sleep_Until(EnterStartDate(CONTRACT_START_DATE_ID, CONTRACT_START_DATE))
-  Sleep_Until(EnterEndDate(CONTRACT_END_DATE_ID, CONTRACT_END_DATE) if ENTER_CONTRACT_END_DATE == "Yes")
-  Sleep_Until(EnterIsUnlimitedUserValue(UNLIMITED_USER_ID) if UNLIMITED_USER_VALUE == "Yes")
+  Sleep_Until(EnterEndDate(CONTRACT_END_DATE_ID, CONTRACT_END_DATE)) if ENTER_CONTRACT_END_DATE == "Yes"
+  Sleep_Until(EnterIsUnlimitedUserValue(UNLIMITED_USER_ID)) if UNLIMITED_USER_VALUE == "Yes"
   Sleep_Until(EnterSubscriberUserValue(SUBSCRIBER_NUMBER_ID, SUBSCRIBER_NUMBER_VALUE))
   Sleep_Until(EnterCurrencyValue(CURRENCY_OPTION_ID, CURRENCY_OPTION_VALUE))
   Sleep_Until(EnterExcessFeeValue(EXCESS_FEE_ID, EXCESS_FEE_VALUE))
-  Sleep_Until(EnterUserToleranceValue(USER_TOLERANCE_ID, USER_TOLERANCE_VALUE) if USER_TOLERANCE_VALUE != "10")
+  Sleep_Until(EnterUserToleranceValue(USER_TOLERANCE_ID, USER_TOLERANCE_VALUE)) if USER_TOLERANCE_VALUE != "10"
   Sleep_Until(EnterProductsSelectionValue(PRODUCT_OPTION_ID, PRODUCT_OPTION_VALUE_1, PRODUCT_OPTION_VALUE_2, PRODUCT_OPTION_VALUE_3))
   Sleep_Until(EnterExceptionsTermsOfPaymentText(PAYMENT_TERMS_ID, PAYMENT_TERMS_VALUE))
   Sleep_Until(EnterStorageAllocationValue(STORAGE_ALLOC_ID, STORAGE_ALLOC_VALUE))
