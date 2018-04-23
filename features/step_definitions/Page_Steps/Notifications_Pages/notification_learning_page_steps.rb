2b76d94eec7ffd9e2571fd583ba3fafd586a74da
shanku_elmo <shankumahanty@elmolearning.com.au>
2018-04-24 09:16:36 +1000

    modified notfication verification logic

--- features/step_definitions/Page_Steps/Notifications_Pages/notification_learning_page_steps.rb
+++ features/step_definitions/Page_Steps/Notifications_Pages/notification_learning_page_steps.rb
@@ -10,29 +10,33 @@ def ConnectToDatabaseAndValidateTheCourseEnrolmentNotification()
     select * from epms_notifier_notification where trigger_id like 'Learning.CourseNewEnrolmentTrigger' ORDER BY id desc LIMIT 1 \\G;")
   begin
     result = %x(mysql -utester -p#{$DB_PWD} #{TMSFULL_DATABASE} -h127.0.0.1 --port 33060 -A < ./features/step_definitions/MySQL_Scripts/sql_commands/learning_course_assignment.sql | tee ./features/step_definitions/MySQL_Scripts/sql_dependencies/myscript.txt) # connect to DB -> run SQL -> save it in text file
-      frs = result.include?  ("recipient_ids: /3472/3456/") #true validate that mail goes to both employee and manager
-      krs = result.include?  ("user_id: 3472") #true validate for employee got enrolled in the course
-      trs = result.include?  ("course_id: 392") #true validate for employee enrolled for the right course
-      mrs = result.include?  ("trigger_id: Learning.CourseNewEnrolmentTrigger") #true validate right template got triggered
-      qrs = result.include?  ("body: <p>New Enrolment<br />
-SYSTEM_PLACEHOLDER_COMPANY_NAME: ELMO<br />
-SYSTEM_PLACEHOLDER_COMPANY_SYSTEM_NAME: HR & Payroll<br />
-SYSTEM_PLACEHOLDER_COMPANY_SHORT_NAME: ELMO<br />
-SYSTEM_PLACEHOLDER_COMPANY_CONTACT: HROnline<br />
-SYSTEM_PLACEHOLDER_COMPANY_EMAIL: noreply@elmolearning.com.au<br />
-PLACEHOLDER_FIRSTNAME: DontTouchAutomationUser<br />
-PLACEHOLDER_LASTNAME: AARON<br />
-PLACEHOLDER_USERNAME: X1242341<br />
-PLACEHOLDER_COURSE: course_section_automation_QuizActivity_shanku<br />
-PLACEHOLDER_COURSE_LINK: https://tmsfull.dev.elmodev.com/learning/view-course/392?_l=1<br />
-PLACEHOLDER_COURSE_DUE_DATE: [not specified]<br />
-&lt;DYNAMIC&gt;PLACEHOLDER_RETRAIN_PERIOD: 0 month&lt;/DYNAMIC&gt; &lt;-- Retrain period should not be displayed here</p>")
-      if frs && krs && trs  && mrs && qrs
+      frs = result.include?  ("recipient_ids: /3472/3456/") || #true validate that mail goes to both employee and manager
+                                 ("user_id: 3472")  || #true validate for employee got enrolled in the course
+                                 ("course_id: 392") || #true validate for employee enrolled for the right course
+                                 ("trigger_id: Learning.CourseNewEnrolmentTrigger") #true validate right template got triggered
+       unless frs == true
+         puts "frs not matching"
+       end
+      qrs = result.include?  ("body: <p>Dear&nbsp;DontTouchAutomationUser&nbsp;AARON&nbsp;REBECCA.AARON@elmodemo.com</p>") ||
+          ("SYSTEM_PLACEHOLDER_COMPANY_NAME: ELMO<br />") ||
+          ("SYSTEM_PLACEHOLDER_COMPANY_SYSTEM_NAME: HR & Payroll<br />") ||
+          ("SYSTEM_PLACEHOLDER_COMPANY_SHORT_NAME: ELMO<br />") ||
+          ("SYSTEM_PLACEHOLDER_COMPANY_CONTACT: HROnline<br />") ||
+          ("SYSTEM_PLACEHOLDER_COMPANY_EMAIL: noreply@elmolearning.com.au<br />") ||
+          ("PLACEHOLDER_FIRSTNAME: DontTouchAutomationUser<br />") ||
+          ("PLACEHOLDER_LASTNAME: AARON<br />") ||
+          ("PLACEHOLDER_USERNAME: X1242341<br />") ||
+          ("PLACEHOLDER_COURSE: course_section_automation_QuizActivity_shanku<br />") ||
+          ("PLACEHOLDER_COURSE_LINK: https://tmsfull.dev.elmodev.com/learning/view-course/392?_l=1<br />") ||
+          ("PLACEHOLDER_COURSE_DUE_DATE: [not specified]<br />")
+    unless qrs == true
+      puts "qrs not matching"
+    end
+      if frs &&  qrs
         puts "Yay! Notification has been triggered"
       else
         raise NotificationException.new("ERROR...Notifications were blocked !!!!!! ")
       end
-
   rescue
     puts "not valid"
 
