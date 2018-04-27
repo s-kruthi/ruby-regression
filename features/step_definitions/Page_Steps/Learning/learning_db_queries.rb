428cebbdaab562cf12acdc5e11ad0b27dbf8e476
Kruthi <kruthi.murthy@elmolearning.com.au>
2018-04-27 17:44:25 +1000

    Created individual db query files and folder structure in Page_Steps

---
+++ features/step_definitions/Page_Steps/Learning/learning_db_queries.rb
@@ -0,0 +1,30 @@
+module Database_env
+  class DAO
+    def get_visible_course_list_by_name(partial_course_name)
+      query = "SELECT id, fullname, shortname, c_template
+               FROM mdl_course
+               WHERE fullname LIKE '%#{partial_course_name}%'
+               AND visible = 1;"
+      return @db[query].first[:fullname]
+    end
+
+
+    def get_course_discrepency_list()
+      query = "SELECT c.fullname courseName, c.id courseId, u.id userId, e.retrain, e.retrain_open
+              FROM epms_lms_course_enrolment e
+              INNER JOIN mdl_course c ON e.course_id = c.id
+              INNER JOIN epms_user u ON e.user_id = u.id
+              WHERE u.is_elmo = 0 AND
+              e.isActive = 1 AND
+              e.status = 2 AND
+              e.retrain != 0 AND
+              c.visible != 0 AND
+              DATE_SUB(DATE_ADD(FROM_UNIXTIME(e.timeEnded), INTERVAL e.retrain MONTH), INTERVAL e.retrain_open DAY) <= CURDATE();"
+      return @db[query].first[:courseName]
+    end
+
+
+  end
+
+
+end
