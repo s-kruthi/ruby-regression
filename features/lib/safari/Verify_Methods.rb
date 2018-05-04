acd03d42d2c4c2c562a37159886a61ab8dffcafb
omar.khan <omar.khan@elmolearning.com.au>
2018-05-04 17:31:32 +1000

    QTA-63 Fixed Screenshot taking process

--- features/lib/safari/Verify_Methods.rb
+++ features/lib/safari/Verify_Methods.rb
@@ -4,7 +4,7 @@ module Safari
     class VerificationException < Exception;
     end
 
-    def VerifyAnElementExistById(id,text)
+    def VerifyAnElementExistById(id, text)
       begin
         wait = Selenium::WebDriver::Wait.new(:timeout => 10)
         select_item = wait.until {
@@ -12,20 +12,20 @@ module Safari
           element if element.displayed?
         }
         expected_text = select_item.text.include? "#{text}"
-        if expected_text == true
+        if expected_text
           puts "\e[0m[ \e[32mPASSED\e[0m ] MATCHED: #{text}"
         else
-          # TODO: Pending review and removal as it’s now redundant and has been added in the After hook
-          # $driver.save_screenshot("./features/Screenshots/#{ENV['CHANNEL']}/screenshot - #{Time.now.strftime('%Y-%m-%d %H-%M-%S')}.png")
-          raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is not matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})")
+          fail
         end
+
       rescue Exception => e
+        Cucumber.wants_to_quit = true
+        raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is NOT matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})\n")
         puts e.message
-        $driver.quit
       end
     end
 
-    def VerifyAnElementExistByClass(class_name,text)
+    def VerifyAnElementExistByClass(class_name, text)
       begin
         wait = Selenium::WebDriver::Wait.new(:timeout => 10)
         select_item = wait.until {
@@ -33,20 +33,20 @@ module Safari
           element if element.displayed?
         }
         expected_text = select_item.text.include? "#{text}"
-        if expected_text == true
+        if expected_text
           puts "\e[0m[ \e[32mPASSED\e[0m ] MATCHED: #{text}"
         else
-          # TODO: Pending review and removal as it’s now redundant and has been added in the After hook
-          # $driver.save_screenshot("./features/Screenshots/#{ENV['CHANNEL']}/screenshot - #{Time.now.strftime('%Y-%m-%d %H-%M-%S')}.png")
-          raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is not matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})")
+          fail
         end
+
       rescue Exception => e
+        Cucumber.wants_to_quit = true
+        raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is NOT matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})\n")
         puts e.message
-        $driver.quit
       end
     end
 
-    def VerifyAnElementExistByXPath(xpath,text)
+    def VerifyAnElementExistByXPath(xpath, text)
       begin
         wait = Selenium::WebDriver::Wait.new(:timeout => 10)
         select_item = wait.until {
@@ -54,20 +54,21 @@ module Safari
           element if element.displayed?
         }
         expected_text = select_item.text.include? "#{text}"
-        if expected_text == true
+        if expected_text
           puts "\e[0m[ \e[32mPASSED\e[0m ] MATCHED: #{text}"
         else
-          # TODO: Pending review and removal as it’s now redundant and has been added in the After hook
-          # $driver.save_screenshot("./features/Screenshots/#{ENV['CHANNEL']}/screenshot - #{Time.now.strftime('%Y-%m-%d %H-%M-%S')}.png")
-          raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is not matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})")
+          fail
         end
+
       rescue Exception => e
+        Cucumber.wants_to_quit = true
+        raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is NOT matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})\n")
         puts e.message
-        $driver.quit
       end
     end
 
-    def VerifyAnElementExistByName(name,text)
+
+    def VerifyAnElementExistByName(name, text)
       begin
         wait = Selenium::WebDriver::Wait.new(:timeout => 10)
         select_item = wait.until {
@@ -75,16 +76,16 @@ module Safari
           element if element.displayed?
         }
         expected_text = select_item.text.include? "#{text}"
-        if expected_text == true
+        if expected_text
           puts "\e[0m[ \e[32mPASSED\e[0m ] MATCHED: #{text}"
         else
-          # TODO: Pending review and removal as it’s now redundant and has been added in the After hook
-          # $driver.save_screenshot("./features/Screenshots/#{ENV['CHANNEL']}/screenshot - #{Time.now.strftime('%Y-%m-%d %H-%M-%S')}.png")
-          raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is not matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})")
+          fail
         end
+
       rescue Exception => e
+        Cucumber.wants_to_quit = true
+        raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is NOT matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})\n")
         puts e.message
-        $driver.quit
       end
     end
 
@@ -96,18 +97,66 @@ module Safari
           element if element.displayed?
         }
         expected_text = select_item.text.include? "#{text}"
-        if expected_text == true
+        if expected_text
           puts "\e[0m[ \e[32mPASSED\e[0m ] MATCHED: #{text}"
         else
-          # TODO: Pending review and removal as it’s now redundant and has been added in the After hook
-          # $driver.save_screenshot("./features/Screenshots/#{ENV['CHANNEL']}/screenshot - #{Time.now.strftime('%Y-%m-%d %H-%M-%S')}.png")
-          raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is not matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})")
+          fail
+        end
+
+      rescue Exception => e
+        Cucumber.wants_to_quit = true
+        raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is NOT matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})\n")
+        puts e.message
+      end
+    end
+
+    ####Verify element and Select2 elements ability to change Martinma123
+    def VerifyElementAbilityByCSS(element_css, setting_ability)
+      begin
+        wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+        element_ability = wait.until {
+          disabled_element_css = "#{element_css}[disabled]"
+          enabled_element_css = element_css
+          if $driver.find_elements(:css, disabled_element_css).any?
+            "disabled"
+          elsif $driver.find_elements(:css, enabled_element_css).any?
+            "enabled"
+          end
+        }
+        if element_ability == setting_ability.downcase
+          puts "\e[0m[ \e[32mPASSED\e[0m ] Element ability MATCHED: #{setting_ability}"
+        else
+          fail
+        end
+
+      rescue Exception => e
+        Cucumber.wants_to_quit = true
+        raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Text is NOT matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})\n")
+        puts e.message
+      end
+    end
+
+    def VerifySelect2Ability(select2_element_css, setting_ability)
+      begin
+        wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+        element_ability = wait.until {
+          disabled_element_css = "#{select2_element_css}.select2-container-disabled"
+          enabled_element_css = "#{select2_element_css}:not(.select2-container-disabled)"
+          if $driver.find_elements(:css, disabled_element_css).any?
+            "disabled"
+          elsif $driver.find_elements(:css, enabled_element_css).any?
+            "enabled"
+          end
+        }
+        if element_ability == setting_ability.downcase
+          puts "\e[0m[ \e[32mPASSED\e[0m ] Element ability MATCHED: #{setting_ability}"
+        else
+          raise VerificationException.new("\e[0m[ \e[31mFAILED\e[0m ] Element ability is not matching. Check screenshot under features->Screenshots->#{ENV['CHANNEL']})")
         end
       rescue Exception => e
         puts e.message
         $driver.quit
       end
     end
-
   end
 end
