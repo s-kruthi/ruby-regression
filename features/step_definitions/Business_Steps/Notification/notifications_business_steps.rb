61d88ebc13b5fe1fe7179699b303869694c66574
omar.khan <omar.khan@elmolearning.com.au>
2018-07-13 14:10:53 +1000

    QTA-53 Modified database_env file and display EPMS LOG MESSAGE ID for better investigation

--- features/step_definitions/Business_Steps/Notification/notifications_business_steps.rb
+++ features/step_definitions/Business_Steps/Notification/notifications_business_steps.rb
@@ -1,6 +1,7 @@
 Then(/^I Should Receive A Notification With Trigger Named (.*) For Recipient (.*)$/i) do |notification_trigger_id, recipient_id|
     @notification_list = $daos.get_notification_by_trigger_id_and_recipient_name(notification_trigger_id, recipient_id)
     puts COLOR_BLUE + "Notification found in the database"
+    puts COLOR_BLUE + "LOG ID: " + @notification_list[:id].to_s
     puts COLOR_BLUE + "Trigger name: " + @notification_list[:trigger_id].to_s
     puts COLOR_BLUE + "Recipient list: " + @notification_list[:recipients].to_s
     puts COLOR_BLUE + "EPMS table Log Status: " + @notification_list[:STATUS].to_s
