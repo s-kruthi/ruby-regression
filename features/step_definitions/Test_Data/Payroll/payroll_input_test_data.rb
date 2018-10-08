2e5284c9999d78f08918f5ec2c1bd6fb1d1898db
Kruthi <kruthi.murthy@elmolearning.com.au>
2018-10-08 16:40:00 +1100

    QTA-88 Removed Elmo Payroll related files since we have a separate repo for the same

--- features/step_definitions/Test_Data/Payroll/payroll_input_test_data.rb
+++
@@ -1,7 +0,0 @@
-#$terminate_emp = "AUTOSUR41 autofirst14"
-ADD_TIMESHEET_EMP = "AUTOSUR59 autofirst11"
-TIMESHEET_PAY_ELEMENT = "Normal Hours"
-INPUT_QUANTITY = 36
-#RATE_OF_PAY = 44
-#COST_CENTRE = "Not applicable"
-TERMINATION_REFERENCE = "Termination reason" + rand(100).to_s
