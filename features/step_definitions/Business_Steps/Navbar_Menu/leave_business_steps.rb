44f842ab5ea92f3dc7ade0bfe02741871af77385
Kruthi <kruthi.murthy@elmolearning.com.au>
2019-02-28 12:57:43 +1100

    PR-1761 Updated tests for taking/banking TIL and LSL leave

--- features/step_definitions/Business_Steps/Navbar_Menu/leave_business_steps.rb
+++ features/step_definitions/Business_Steps/Navbar_Menu/leave_business_steps.rb
@@ -2,26 +2,36 @@ And(/^I Can Select (.*) Leave Type$/i) do | leave_type |
   sleep (4)
   SelectFromDropdown(LEAVE_REQUEST_LEAVETYPE_ID, leave_type)
 
-  date_val = (DateTime.now + (rand * 7)).strftime "%d/%m/%Y"
+  if @bank_leave == true
+    #saying you worked on a Saturday
+    date_val = (DateTime.now + (rand * 7))
+    date_val = date_val.beginning_of_week(:saturday).strftime "%d/%m/%Y"
+    puts (COLOR_BLUE + "banking leave on " + date_val).upcase
+
+  else
+    date_val = (DateTime.now + (rand * 7)).strftime "%d/%m/%Y"
+    puts (COLOR_BLUE + "taking leave on " + date_val).upcase
+  end
+
   SelectLeaveDate(date_val)
 
   EnterLeaveHours(LEAVE_REQUEST_HOURS_VALUE)
-  byebug
 end
 
 
 And(/^I Choose To (Bank|Take) The Request$/i) do | leave_action |
   if leave_action == 'Bank'
     JavascriptClick(LEAVE_REQUEST_TOGGLE_ID)
+    @bank_leave = true
   else
     puts COLOR_BLUE + "toggle is by default set to take".upcase
+    @bank_leave = false
   end
 end
 
 
 Then(/^I Should Be Able To See That My Leave Request Was Successfully Submitted$/i) do
   ClickElement('xpath', LEAVE_REQUEST_SUBMIT_ID)
-  byebug
 
   clicked_at = (Time.now).strftime "%d/%m/%Y %I:%M:%S %p"
   puts COLOR_BLUE + ("submitted leave request at " + clicked_at).upcase
@@ -73,14 +83,14 @@ end
 
 
 Then(/^I Should Be Able To See That My Leave Request Cannot Be Submitted$/i) do
-  byebug
+
   ClickElement('xpath', LEAVE_REQUEST_SUBMIT_ID)
+  pending 'Because of a bug PR-2114'
 
   sleep(2)
   err_msg_text = $driver.find_elements(:xpath,'//div[contains(@class, "alert-danger")]//span[contains(@class, "ng-binding")]')[1].text
 
   expect(err_msg_text).to eq('Leave request hours cannot exceed the leave balance.')
   puts COLOR_GREEN + "insufficient leave balance error is displayed".upcase
-  byebug
 end
 
