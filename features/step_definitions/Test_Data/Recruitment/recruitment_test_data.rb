0887299d4299d8ca8c58255a59d755fc3d002873
Subi Deb <subi.deb@elmosoftware.com.au>
2019-04-02 14:51:01 +1100

    QTA-80 Added tests for creating new candidate and search candidate

--- features/step_definitions/Test_Data/Recruitment/recruitment_test_data.rb
+++ features/step_definitions/Test_Data/Recruitment/recruitment_test_data.rb
@@ -5,11 +5,12 @@ NUMBER_OF_POSITION_INPUT = '0'
 REQUISITION_DUEDATE = "31/12/2017"
 REQUISITION_MESSAGE_BODY = 'This is a test Requisition message.\n Please do not delete this.\n Thank you'
 
+
 #Recruitment New Candidate Test Data
 NEW_CANDIDATE_BTN_LABEL = 'New Candidate'
 CANDIDATE = Hash.new
-CANDIDATE[:FIRST] = "Test #{SecureRandom.hex(5)}"
-CANDIDATE[:LAST] = "Team #{SecureRandom.hex(5)}"
+CANDIDATE[:FIRST] = "Test"#{SecueRandom.hex(5)}"
+CANDIDATE[:LAST] = "Team" #{SecureRandom.hex(5)}"
 CANDIDATE[:EMAIL] = "test.team"
 CANDIDATE[:HOMEPHONE] = "80603700"
 CANDIDATE[:MOBILE] = "0430222301"
@@ -20,6 +21,7 @@ CANDIDATE[:COUNTRY] = "Australia"
 CANDIDATE[:STATE] = "NSW"
 CANDIDATE[:TIMEZONE] = "Sydney"
 CANDIDATE[:PASSWORD] = "Tester1!"
+CANDIDATE_UPDATE_SUCCESS_MSG = "Profile updated successfully."
 
 
 #Recruitment create job ad test data #######
@@ -27,13 +29,13 @@ SALARY_FROM_VALUE = "100000"
 SALARY_TO_VALUE = "200000"
 JB_END_DATE_VALUE = "29/05/2019"
 
+
 #### REQUISITION NOTE TEST DATA ####
 REQUISITION_NOTE_SUCCESS_MSG = 'Requisition note has been successfully updated.'
 REQUISITION_NOTE_VALUE = "Automated Test Requisition Note"
 REQUISITION_NOTE_DEL_MSG = 'Requisition note has been successfully deleted.'
-
-
 PDF_FILE_NAME = "sample.pdf"
+
 
 #### REQUISITION MAIL TO CANDIDATE ####
 REQUISITION_EMAILCANDIDATE_SUBJ_VALUE = 'Automated Send Mail'
