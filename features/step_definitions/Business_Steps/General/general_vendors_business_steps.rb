db722cbc94feed28ee65d66182a79848bc5a2926
Kruthi <kruthi.murthy@elmolearning.com.au>
2019-06-13 10:40:33 +1000

    QTA-106 Completed the test for adding a vendor user

--- features/step_definitions/Business_Steps/General/general_vendors_business_steps.rb
+++ features/step_definitions/Business_Steps/General/general_vendors_business_steps.rb
@@ -5,12 +5,10 @@ And(/^I Can Add A Vendor$/i) do
   Sleep_Until(ClickElement('id','vendor_save'))
 end
 
-
-Then(/^I Should See That Vendor Is Successfully Added$/i) do
+Then(/^I Should See That Vendor Is Added Successfully$/i) do
   VerifySuccessAlertMessage(VERIFY_SAVE_SUCCESSFUL_ID, 'Vendor has been successfully added.')
   expect(GetTextAssociatedToElement("xpath","//ol[@class='breadcrumb']/child::li[@class='active']")).to eq('testing_vendors')
 end
-
 
 When(/^I Search For "([\w\s]+)" Vendor$/i) do | vendor_name |
   @vendor_details = $daos.get_vendor_details(vendor_name)
@@ -23,7 +21,6 @@ When(/^I Search For "([\w\s]+)" Vendor$/i) do | vendor_name |
   end
 end
 
-
 And(/^I Edit The Name To Be "([\w\s]+)"$/i) do | edit_name |
   @edited_name = edit_name
   Sleep_Until(ClickElement("xpath", "//a[contains(@href, '/edit/#{@vendor_details[:id]}')]"))
@@ -32,8 +29,36 @@ And(/^I Edit The Name To Be "([\w\s]+)"$/i) do | edit_name |
   ClickOnSaveButton(SAVE_BTN_ID)
 end
 
-
 Then(/^I Should See That The Vendor Was "([\w]+)" Successfully$/i) do | action |
   VerifySuccessAlertMessage(VERIFY_SAVE_SUCCESSFUL_ID, 'Vendor has been successfully updated.')
 end
+
+And(/^I Choose To "([\w\s]+)" For The Vendor$/i) do | action |
+  # click on the action dropdown
+  $driver.find_element(:xpath, "//button[contains(@class, 'dropdown-toggle')]").click
+
+  case action
+    when "Add Vendor User"
+      identifier = "//a[@href='/admin/vendor/" + @vendor_details[:id].to_s + "/user/new/']"
+  end
+  $driver.find_element(:xpath, identifier).click
+end
+
+And(/^I Enter The Vendor User Details$/i) do
+  WaitForAnElementByIdAndInputValue('vendoruser_firstName', 'testing_vendors')
+  WaitForAnElementByIdAndInputValue('vendoruser_lastName', 'testing_vendors')
+
+  suffix = Time.now.strftime("%Y%m%d%H%M%S").to_s
+  vendor_user_email = 'test'+ suffix +'@elmodev.com'
+  WaitForAnElementByIdAndInputValue('vendoruser_email', vendor_user_email)
+  
+  WaitForAnElementByIdAndTouch('vendoruser_save')
+end
+
+Then(/^I Should See That The Vendor User Is Added Successfully$/i) do
+  VerifySuccessAlertMessage(VERIFY_SAVE_SUCCESSFUL_ID, 'User has been successfully added.')
+end
+
+
+
 
--- features/tests/General/general_vendors.feature
+++ features/tests/General/general_vendors.feature
@@ -10,14 +10,24 @@ Feature:
     When  I go to Admin Settings
     And   I Go To "Vendors" under "General" section
     And   I Can Add A Vendor
-    Then  I Should See That Vendor Is Successfully Added
-
+    Then  I Should See That Vendor Is Added Successfully
 
   @general_low_risk @vendor_edit
-  Scenario: [Recruitment]Can Edit Vendor
+  Scenario: [Recruitment]Can Edit A Vendor
     Given I Have Logged In as a Company Admin
     And   I go to Admin Settings
     And   I Go To "Vendors" under "General" section
     When  I Search For "testing_vendors" Vendor
     And   I Edit The Name To Be "Testing Vendors"
     Then  I Should See That The Vendor Was "Edited" Successfully
+
+  @general_low_risk @vendor_user_add
+  Scenario: [Recruitment]Can Add A Vendor User
+    Given I Have Logged In as a Company Admin
+    And   I go to Admin Settings
+    And   I Go To "Vendors" under "General" section
+    When  I Search For "Testing Vendors" Vendor
+    And   I Choose To "Add Vendor User" For The Vendor
+    And   I Enter The Vendor User Details
+    Then  I Should See That The Vendor User Is Added Successfully
+
