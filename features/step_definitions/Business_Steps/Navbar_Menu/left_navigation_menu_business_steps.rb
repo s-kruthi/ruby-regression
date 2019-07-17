e793dde951b96e3f9966d8326a30e2b45b1e1a6d
Omar Khan <omar.khan@elmolearning.com.au>
2019-07-17 15:25:46 +1000

    QTA-124 Fixed Code conflict

--- features/step_definitions/Business_Steps/Navbar_Menu/left_navigation_menu_business_steps.rb
+++ features/step_definitions/Business_Steps/Navbar_Menu/left_navigation_menu_business_steps.rb
@@ -5,6 +5,7 @@ end
 Then(/^I Should Be Able To Click "([^"]*)" From Left Navigation Menu/i) do |arg|
   identifier = "//span[@class='menuitem-link__label'][contains(.,'#{arg}')]"
   Sleep_Until(WaitForAnElementByXpathAndTouch(identifier))
-  sleep 3 #Using explicit sleep as the page is taking longer than expected to load up
+  #Using explicit sleep as the page is taking longer than expected to load up
+  sleep 3
   Sleep_Until(VerifyAnElementExists('xpath', "//div[@style='font-size: 24px;'][contains(.,'#{arg}')]"))
 end
